b137a5ad9824da8ec309a678c01e198a7f1591ed
WalterMaranini <115894074+WalterMaranini@users.noreply.github.com>
2024-03-03 19:53:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response, request
+
+# import camera driver
+#if os.environ.get('CAMERA'):
+#    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+#else:
+#from camera import Camera
+
+# Raspberry Pi camera module (requires picamera package)
+from camera_pi import Camera
+
+app = Flask(__name__)
+Acceso=True
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """Video streaming home page."""
